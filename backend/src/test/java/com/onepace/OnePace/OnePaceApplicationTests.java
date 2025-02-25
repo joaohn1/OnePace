962008dcc6bef96bcfa1af92c961c8c38028a405
Joao Vitor <joaohn1@hotmail.com>
2025-02-25 20:58:00 -0300

    PostgreSQL connection and import Spring package

---
+++ backend/src/test/java/com/onepace/OnePace/OnePaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onepace.OnePace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnePaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
